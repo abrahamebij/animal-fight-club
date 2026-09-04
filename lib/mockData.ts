4a3b9def3eb5cb7e331645b9e78cac93f69ce896
Abraham Ebijuni <abreb1235@gmail.com>
2026-09-04 11:49:00 +0100

    chore: delete mockData.ts

--- lib/mockData.ts
+++
@@ -1,6 +0,0 @@
-import { Beast, Battle, LeaderboardEntry, BettorLeaderboardEntry } from '@/lib/types';
-
-export const MOCK_BEASTS: Beast[] = [];
-export const MOCK_BATTLES: Battle[] = [];
-export const MOCK_LEADERBOARD_BEASTS: LeaderboardEntry[] = [];
-export const MOCK_LEADERBOARD_BETTORS: BettorLeaderboardEntry[] = [];
